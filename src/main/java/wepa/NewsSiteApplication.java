bf8c59cdbca5c2f06cfb5818c7bec361b1010bd7
Arttu <arttukuronen@gmail.com>
2017-11-29 09:56:53 +0200

    first commit

---
+++ src/main/java/wepa/NewsSiteApplication.java
@@ -0,0 +1,12 @@
+package wepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSiteApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(NewsSiteApplication.class, args);
+    }
+}
---
+++ src/main/java/wepa/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package wepa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DefaultController {
+    
+    @GetMapping("*")
+    @ResponseBody
+    public String hello(){
+        return "Hello world!";
+    }
+}
